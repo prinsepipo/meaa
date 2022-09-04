59878c04253d64be6370e365cebfca2a090f18a0
Pipo <prinsepipo.sanchez@gmail.com>
2022-09-04 17:03:15 +0800

    Add Info with message at the top of the page

---
+++ src/components/Info.jsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import "./Info.scss";
+
+
+const Info = () => {
+  return (
+    <div className="info">
+      <div className="wrapper">
+        <p className="info-message">This site is just a prototype. It does not, in any way, represent the school Mandaue Ebenezer Alliance Academy (MEAA).</p>
+      </div>
+    </div>
+  );
+};
+
+
+export default Info;
--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -2,15 +2,17 @@ import * as React from "react";
 
 import PopupProvider from "../context/popup";
 
-import Popup from "../components/popups/Popup";
+import Info from "../components/Info";
 import Header from "./Header";
 import Footer from "./Footer";
+import Popup from "../components/popups/Popup";
 
 
 const Default = (props) => {
   return (
     <PopupProvider>
       <div className="default">
+        <Info />
         <Header />
 
         {props.children}
